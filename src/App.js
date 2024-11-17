a44ed09e9bdda9cbba965d0e04288a188bc39172
Troy Washington <troyvw96@gmail.com>
2024-11-16 21:26:51 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import { Line } from 'react-chartjs-2';
+import 'chart.js/auto';
 
 function App() {
+  const [stockData, setStockData] = useState([]);
+  const APIkey = '4ee2d16b8b906c8743879b5fcd5b2415';
+  const url = `http://api.marketstack.com/v1/eod?access_key=${APIkey}&symbols=AAPL`;
+
+
+  useEffect(() => {
+    const fetchStockData = async () => {
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        const total = result.pagination.total;
+
+        const fullDataResponse = await fetch(`${url}&limit=${total}`);
+        const fullDataResult = await fullDataResponse.json();
+
+        setStockData(fullDataResult.data);
+        console.log("Stock data:", stockData);
+      } catch (error) {
+        console.error("Error fetching stock data:", error);
+      }
+    };
+
+    fetchStockData();
+  }, []);
+  const data = {
+    labels: stockData.map(entry => entry.date.split('T')[0]),
+    datasets: [
+      {
+        label: 'AAPL Stock Price',
+        data: stockData.map(entry => entry.close),
+        fill: false,
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)',
+      },
+    ],
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>AAPL Stock Price</h1>
+      <Line data={data} />
     </div>
   );
 }
